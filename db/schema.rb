5d48d7f1f653bb1f8bccba17d79a576341f82152
natachas <natacha.springer@gmail.com>
2014-07-24 16:48:38 -0400

    set up theme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724180326) do
+ActiveRecord::Schema.define(version: 20140724192731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20140724180326) do
     t.datetime "updated_at"
   end
 
+  create_table "user_stocks", force: true do |t|
+    t.integer "user_id"
+    t.integer "stock_id"
+  end
+
   create_table "users", force: true do |t|
     t.string   "provider"
     t.string   "uid"
@@ -34,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140724180326) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "profile_pic"
   end
 
 end
