04c5f98ee103d3956dd5a1422ec46daa37912f79
Mandy Yeung <mandycodes@gmail.com>
2014-07-24 16:54:18 -0400

    Started stocktwit feed method

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,21 @@
+require 'net/http'
+
 class Stock < ActiveRecord::Base
   has_many :user_stocks
   has_many :users, through: :user_stocks
+
+  def stocktwits_feed(ticker)
+      source = %{https://api.stocktwits.com/api/2/streams/symbol/#{ticker}.json}
+      resp = Net::HTTP.get_response(URI.parse(source))
+      data = resp.body
+      result = JSON.parse(data)["messages"]
+      result.each do |msg|
+        @user = msg["user"]["username"]
+        @user_pic = msg["user"]["avatar_url"]
+        @body = msg["body"]
+        @time = msg["create_at"]
+        # @discuss = msg["conversation"]["replies"]
+        # @likes = msg["likes"]["total"] || nil
+      end
+  end
 end
