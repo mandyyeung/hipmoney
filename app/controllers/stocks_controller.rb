dddf4342882fabac899e5d2aaa3f79f688e44e5c
mendyismyname <seldo6@gmail.com>
2014-07-25 15:41:47 -0400

    autofill

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,14 @@
 class StocksController < ApplicationController
+  def index
+    @stocks = Stock.where('name LIKE ? OR ticker LIKE ?', "%#{params[:query]}%", "%#{params[:query]}%")
+    respond_to do |format|
+      format.json { render json: @stocks }
+    end
+  end
+
+  private 
+
+  def stock_params
+     params.require(:stock).permit(:query)
+  end
 end
