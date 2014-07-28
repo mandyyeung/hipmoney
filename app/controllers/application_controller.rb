620235e1e8b17b6c42c25f5dd9e93bdd568137ad
mendyismyname <seldo6@gmail.com>
2014-07-28 17:16:20 -0400

    autofill validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-
+  protect_from_forgery except: :show
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
