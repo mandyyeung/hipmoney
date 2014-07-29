68c3cfb86c68b4dad4a8a5b4698cf3a20c2a88cc
natachas <natacha.springer@gmail.com>
2014-07-29 17:12:29 -0400

    started video login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/show'
 
-  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:provider/callback', to: 'sessions#create', as: 'signin'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
