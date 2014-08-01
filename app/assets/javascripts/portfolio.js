f8c274d561cfca4d53828296313a6d8499057bd3
Mandy Yeung <mandycodes@gmail.com>
2014-08-01 16:04:10 -0400

    Working on portfolio

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,5 +1,5 @@
 $(document).on('page:change', function () {
-  $('.portfolio-edit').click(function(){
+  $('#portfolio-edit').click(function(){
     $('.portfolio').load('/users/edit_portfolio');
   });
 });
